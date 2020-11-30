6221083cce79ae8f048f30fd410843498a3418eb
koduru5 <68308123+koduru5@users.noreply.github.com>
2020-11-29 21:49:03 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var gameState=0;
+var playerCount;
+var database;
+var form,player,game;
+var allPlayers;
+var distance=0;
+var cars, car1, car2, car3, car4;
+
+function setup(){
+    createCanvas(displayWidth-20,displayHeight-30);
+    //creating the database
+    database= firebase.database();
+    game=new Game();
+    game.getState();
+    game.start();
+}
+
+function draw(){
+    if(playerCount===4) {
+        game.update(1);
+    }
+    if(gameState===1) {
+        clear();
+        game.play();
+    }
+}
